afdf49b59eb243369248e716a836d2d80e163b5e
Jason Kadman <jkadman231@gmail.com>
2024-02-04 23:06:13 -0800

    fix minor syntax errors, basic command handling established

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
+// console.log('foldersPath', foldersPath)
 const commandFolders = fs.readdirSync(foldersPath);
+// console.log('commandFolders', commandFolders)
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
+  // console.log('commandsPath', commandsPath);
   const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  console.log('commandFiles', commandFiles)
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name. command)
+      client.commands.set(command.data.name, command)
     } else {
       console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
@@ -35,7 +39,7 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 // Login to Discord with your client's token
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
