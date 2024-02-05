b75fa9af651ba9eee7514bd2a523a2ec28f9fa61
Jason Kadman <jkadman231@gmail.com>
2024-02-04 21:51:42 -0800

    create basic command file and interactions

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 // require('dotenv').config()
 
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('dotenv').config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(_dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name. command)
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+}
 
 // When the client is ready, run this code (only once)
 client.once(Events.ClientReady, readyClient => {
@@ -14,3 +36,8 @@ client.once(Events.ClientReady, readyClient => {
 
 // Login to Discord with your client's token
 client.login(token);
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
