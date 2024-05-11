fe1afae9903cae3fb6fd5b6c8ef172fd8aa440c7
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-11 16:26:06 -0700

    change slash command to access db

--- commands/utility/addInfo.js
+++ commands/utility/addInfo.js
@@ -1,17 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { addCharacter } = require('/home/crow/Documents/programming/osebot/database/commands.js');
 
-let list = ['sword', ' shield'];
+// let list = ['sword', ' shield'];
 
 // idea for adding gear, will use in conjunction with SQL to add item to database
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('addgear')
-		.setDescription('Add gear to the commununal gear list.'),
+		.setName('addcharacter')
+		.setDescription('Add Character to the characters database.'),
 	async execute(interaction) {
-    let test = [0];
 
-    await interaction.reply('what would you like to add?')
-    await interaction.followUp('your item has been added')
+		const characterName = interaction.options.getString('characterName');
+		const characterLevel = interaction.options.getString('level')
+		// async execute()
+    // let test = [0];
+		try {
+			await addCharacter(interaction, characterName, characterLevel);
+
+			// send confirmation
+			await interaction.reply('Character successfully added');
+		} catch (error) {
+			// handle error
+			console.error('Error adding gear: ', error);
+			await interaction.reply('Failed to add character');
+		}
+    
+    // await interaction.followUp('your item has been added')
       
     
 		
