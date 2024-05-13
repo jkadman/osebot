ce203ea4a6c0ab37c161150dbb5f52e428564d40
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-13 00:09:52 -0700

    remove uneeded or commented out code

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,12 +7,9 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-// const foldersPath = path.join(__dirname, 'commands');
-// const commandFolders = fs.readdirSync(foldersPath);
 
+// look at all commands in the commands folder
 const commandsPath = path.join(__dirname, 'commands');
-
 const commandFiles = fs.readdirSync(commandsPath).filter(file=>file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -25,26 +22,6 @@ for (const file of commandFiles) {
 	}
 }
 
-// for (const folder of commandFolders) {
-// 	// Grab all the command files from the commands directory you created earlier
-// 	const foldersPath = path.join(__dirname, 'commands');
-// 	const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
-// 	console.log('folder: ', folder);
-// 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-// 	for (const file of commandFiles) {
-// 		const filePath = path.join(foldersPath, file);
-// 		const command = require(filePath);
-// 		console.log('file: ', file)
-// 		if ('data' in command && 'execute' in command) {
-// 			console.log(`Registering command: ${command.data.name}`);
-// 			commands.push(command.data.toJSON());
-// 		} else {
-// 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-// 		}
-// 	}
-// }
-console.log('clientid', clientId);
-console.log('guildid', guildID);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
